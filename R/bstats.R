9b421974c3e28ca8357c5bd4e3446925ca359945
Roseco-crs <56065185+Roseco-crs@users.noreply.github.com>
2021-03-26 13:02:18 +0200

    Add files via upload

---
+++ R/bstats.R
@@ -0,0 +1,26 @@
+#' Create histogram, boxplot, and Numerical summary
+#' @export
+#' @param  x Numerical variable
+#' @author Coffi Rodolphe Segbedji
+#' @description Basic statistics descriptions
+
+
+stat <- function(x) {
+  # 1 row and 2 colmuns
+  par(mfrow = c(1,2))
+
+  # Histogram
+  hist(x, col= rainbow(30))
+
+  # Box plot
+  boxplot(x, col= 'green')
+  par(mfrow = c(1,1))
+
+  # Numeric summary
+  data.frame(min = min(x),
+             median = median(x),
+             mean= mean(x),
+             max= max(x))
+
+
+}
